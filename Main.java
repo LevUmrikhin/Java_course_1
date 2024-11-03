f8078b77e6ede99d219bd6fb55ce869121e4a114
LevUmrikhin <leoumr0221@icloud.com>
2024-11-03 09:08:35 +0000

    Add files via upload
    numbers between 1 and 100, divisible by 3

---
+++ Main.java
@@ -0,0 +1,10 @@
+package ru.isys.trainings.task2;
+public class Main {
+    public static void main(String[] args){
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 ==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
