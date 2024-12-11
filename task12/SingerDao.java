03d51a66314fa407f82512ea79a9fa14d230e8ad
LevUmrikhin <leoumr0221@icloud.com>
2024-12-11 15:41:38 +0000

    Delete task12 directory

--- task12/SingerDao.java
+++
@@ -1,9 +0,0 @@
-package task12;
-import task12.realClasses.Singer;
-
-import java.util.List;
-
-public interface SingerDao {
-    public List<Singer> findSingers() throws InterruptedException;
-    public void saveSingers(List<Singer> singers) throws InterruptedException;
-}
